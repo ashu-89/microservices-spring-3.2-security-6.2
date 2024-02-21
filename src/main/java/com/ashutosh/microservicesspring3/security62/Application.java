d2cd9d9dd77a159615618cae469faf1d143e9a65
Ashutosh Pathak <ashu.p89@gmail.com>
2024-02-22 04:17:05 +0530

    initial commit - parent of multi module

---
+++ src/main/java/com/ashutosh/microservicesspring3/security62/Application.java
@@ -0,0 +1,13 @@
+package com.ashutosh.microservicesspring3.security62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashutosh/microservicesspring3/security62/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.microservicesspring3.security62;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
